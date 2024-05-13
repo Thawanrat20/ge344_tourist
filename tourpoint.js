0f85cad90911060bf2e71d8e63064958ef03a26e
Thawanrat20 <158019588+Thawanrat20@users.noreply.github.com>
2024-05-13 16:35:27 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,158 @@
+
+
+var tourpoint=
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao Yai Netional Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.50404083853113,
+            14.333843367830141
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "grand palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49266868905255,
+            13.751516205984203
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "icon siam",
+          "type": "spacial",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.51043649999929,
+            13.726690655187085
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "pak khlong market",
+          "type": "market",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49636698486319,
+            13.741808962003162
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "bueng borraphet",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.23969991209822,
+            15.693774156133642
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khlong tha dan dam Reservoir",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            101.32030110344976,
+            14.313553409652556
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "pattaya",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.88289628378078,
+            12.936870142941586
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ban laem",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            99.98993471888957,
+            13.226496537722625
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ca am",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.98039070612339,
+            12.791383124523335
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "hua hin",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.9584612151886,
+            12.569708547945496
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
